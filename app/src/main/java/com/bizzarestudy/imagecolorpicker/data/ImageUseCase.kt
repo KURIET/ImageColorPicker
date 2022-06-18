6c742377f445101dc023ca67e4e3fc2c9c25bd27
boring-km <kms0644804@naver.com>
2022-06-18 23:13:35 +0900

    [KM-01] 코드 리팩토링

--- app/src/main/java/com/bizzarestudy/imagecolorpicker/data/ImageUseCase.kt
+++ app/src/main/java/com/bizzarestudy/imagecolorpicker/data/ImageUseCase.kt
@@ -1,83 +1,17 @@
 package com.bizzarestudy.imagecolorpicker.data
 
-import android.content.ContentValues
 import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.graphics.ImageDecoder
 import android.net.Uri
 import android.os.Build
-import android.os.Environment
 import android.provider.MediaStore
-import androidx.core.net.toUri
 import com.bizzarestudy.imagecolorpicker.domain.model.PixelColor
-import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
-import java.util.*
+import com.bizzarestudy.imagecolorpicker.presentation.pixels.PixelCanvas
+import kotlin.math.roundToInt
 
 object ImageUseCase {
-
-    @Throws(IOException::class)
-    fun saveBitmap(
-        context: Context, bitmap: Bitmap, fileSegName: String
-    ): Uri {
-
-        val displayName = "${fileSegName}_pixel"
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            // https://stackoverflow.com/questions/56904485/how-to-save-an-image-in-android-q-using-mediastore
-            val values = ContentValues().apply {
-                put(MediaStore.MediaColumns.DISPLAY_NAME, displayName)
-                put(MediaStore.MediaColumns.MIME_TYPE, "image/png")
-                put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DCIM)
-            }
-
-            var uri: Uri? = null
-
-            return runCatching {
-                with(context.contentResolver) {
-                    insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)?.also {
-                        uri = it // Keep uri reference so it can be removed on failure
-
-                        openOutputStream(it)?.use { stream ->
-                            if (!bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream))
-                                throw IOException("Failed to save bitmap.")
-                        } ?: throw IOException("Failed to open output stream.")
-
-                    } ?: throw IOException("Failed to create new MediaStore record.")
-                }
-            }.getOrElse {
-                uri?.let { orphanUri ->
-                    // Don't leave an orphan entry in the MediaStore
-                    context.contentResolver.delete(orphanUri, null, null)
-                }
-
-                throw it
-            }
-        } else {
-            @Suppress("DEPRECATION")
-            val root = Environment.getExternalStorageDirectory().toString() + "/color_picker/"
-            val myDir = File(root).apply { mkdirs() }
-            val file = File(myDir, "$displayName.png")
-            if (file.exists()) {
-                file.delete()
-            }
-            try {
-                val out = FileOutputStream(file)
-                bitmap.compress(Bitmap.CompressFormat.PNG, 100, out)
-                out.flush()
-                out.close()
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-            return file.toUri()
-        }
-    }
-
-    private fun getRandom(): Int {
-        val random = Random(1000)
-        return random.nextInt(1000) + 1
-    }
 
     fun abgrToColor(argbColor: Int): PixelColor {
         val a: Int = argbColor shr 24 and 0xff // or color >>> 24
@@ -96,5 +30,47 @@ object ImageUseCase {
     } else {
         val source = ImageDecoder.createSource(context.contentResolver, uri!!)
         ImageDecoder.decodeBitmap(source)
+    }
+
+    fun drawPixels(
+        context: Context,
+        bitmap: Bitmap,
+        xCount: Int,
+        yCount: Int,
+        pixel: Double,
+        selectedPixel: Int
+    ): Bitmap {
+        val bitmapToSave = Bitmap.createBitmap(
+            (xCount * pixel).roundToInt(),
+            (yCount * pixel).roundToInt(), Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(bitmapToSave)
+        val pixelCanvas = PixelCanvas(context)
+
+        // colors 가상 캔버스에 세팅
+        pixelCanvas.set(getColorsFrom(bitmap, selectedPixel), xCount, yCount)
+
+        // 가상 캔버스에 픽셀 이미지 그리기
+        pixelCanvas.draw(canvas)
+        return bitmapToSave
+    }
+
+    private fun getColorsFrom(bitmap: Bitmap, selectedPixel: Int): ArrayList<PixelColor> {
+        val xCount = selectedPixel - 1
+        val yCount = getCustomHeight(bitmap, xCount)
+        val colors = arrayListOf<PixelColor>()
+        val chunk = bitmap.width / (xCount + 1)
+
+        for (y in 0 until yCount) {
+            for (x in 0 until xCount) {
+                val pixelColor = bitmap.getPixel(x * chunk, y * chunk)
+                colors.add(abgrToColor(pixelColor))
+            }
+        }
+        return colors
+    }
+
+    fun getCustomHeight(bitmap: Bitmap, pixel: Int): Int {
+        return (pixel * (bitmap.height.toDouble() / bitmap.width)).toInt()
     }
 }
